fa47b1037722eba01752cf4e5dde4154764cc5d5
ClearEyesFullHearts <mathieu.brick@gmail.com>
2023-09-25 11:10:32 +0200

    extension forced check for worker

--- trust/extension/service-worker.js
+++ trust/extension/service-worker.js
@@ -7,80 +7,6 @@ let memory = {};
 const { COMMIT, BASE_URL, ...map } = config;
 let resultMap = [];
 
-function onClientReady(request) {
-  if (request.action.type === 'READY') {
-    if (isAttached) {
-      chrome.tabs.sendMessage(tabId, {
-        action: {
-          type: 'RESULT',
-          data: resultMap,
-        },
-      });
-      resultMap = [];
-    }
-    scriptReady = true;
-  }
-}
-function onURLChange(id, changeInfo) {
-  const { url, status } = changeInfo;
-  if (isAttached && status === 'loading' && url && url !== BASE_URL) {
-    chrome.debugger.detach({ tabId: id }).then(() => {
-      onDetaching();
-    });
-  }
-}
-function onDetaching() {
-  isAttached = false;
-  scriptReady = false;
-  memory = {};
-  chrome.runtime.onMessage.removeListener(onClientReady);
-  chrome.tabs.onUpdated.removeListener(onURLChange);
-}
-
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url === BASE_URL) {
-    if (isAttached) {
-      chrome.debugger.detach({ tabId: tab.id }).then(() => {
-        onDetaching();
-      });
-    } else {
-      chrome.debugger.attach({ tabId: tab.id }, '1.2')
-        .then(() => {
-          tabId = tab.id;
-          isAttached = true;
-          chrome.debugger.sendCommand(
-            { tabId: tab.id },
-            'Network.enable',
-            {},
-          )
-            .then(() => {
-              if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-              } else {
-                chrome.tabs.sendMessage(tabId, {
-                  action: {
-                    type: 'START',
-                  },
-                }).catch((err) => {
-                  console.log('Not ready yet');
-                });
-                chrome.runtime.onMessage.addListener(onClientReady);
-                chrome.tabs.onUpdated.addListener(onURLChange);
-              }
-            });
-        });
-    }
-  } else {
-    console.log('Debugger can only be attached to ySyPyA Home Page.', tab.url);
-  }
-});
-
-chrome.debugger.onDetach.addListener(
-  () => {
-    onDetaching();
-  },
-);
-
 function responseResult(url, hash) {
   if (!map[url]) {
     if (scriptReady) {
@@ -157,6 +83,87 @@ function ToBinary(txt) {
 
   return result;
 }
+
+function onClientReady(request) {
+  if (request.action.type === 'READY') {
+    if (isAttached) {
+      chrome.tabs.sendMessage(tabId, {
+        action: {
+          type: 'RESULT',
+          data: resultMap,
+        },
+      });
+      fetch(`${BASE_URL}worker/sw.js`)
+        .then((response) => response.text())
+        .then((sw) => getHash(btoa(ToBinary(sw))))
+        .then((hash) => {
+          console.log('loadingFinished', `${BASE_URL}worker/sw.js`);
+          responseResult(`${BASE_URL}worker/sw.js`, hash);
+        });
+      resultMap = [];
+    }
+    scriptReady = true;
+  }
+}
+function onURLChange(id, changeInfo) {
+  const { url, status } = changeInfo;
+  if (isAttached && status === 'loading' && url && url !== BASE_URL) {
+    chrome.debugger.detach({ tabId: id }).then(() => {
+      onDetaching();
+    });
+  }
+}
+function onDetaching() {
+  isAttached = false;
+  scriptReady = false;
+  memory = {};
+  chrome.runtime.onMessage.removeListener(onClientReady);
+  chrome.tabs.onUpdated.removeListener(onURLChange);
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url === BASE_URL) {
+    if (isAttached) {
+      chrome.debugger.detach({ tabId: tab.id }).then(() => {
+        onDetaching();
+      });
+    } else {
+      chrome.debugger.attach({ tabId: tab.id }, '1.2')
+        .then(() => {
+          tabId = tab.id;
+          isAttached = true;
+          chrome.debugger.sendCommand(
+            { tabId: tab.id },
+            'Network.enable',
+            {},
+          )
+            .then(() => {
+              if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+              } else {
+                chrome.tabs.sendMessage(tabId, {
+                  action: {
+                    type: 'START',
+                  },
+                }).catch((err) => {
+                  console.log('Not ready yet');
+                });
+                chrome.runtime.onMessage.addListener(onClientReady);
+                chrome.tabs.onUpdated.addListener(onURLChange);
+              }
+            });
+        });
+    }
+  } else {
+    console.log('Debugger can only be attached to ySyPyA Home Page.', tab.url);
+  }
+});
+
+chrome.debugger.onDetach.addListener(
+  () => {
+    onDetaching();
+  },
+);
 
 chrome.debugger.onEvent.addListener((source, method, params) => {
   if (method === 'Network.responseReceived') {
